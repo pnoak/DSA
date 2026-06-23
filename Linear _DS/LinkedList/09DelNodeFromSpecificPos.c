9440a05eb46d0fac2a872f88b210d49b699f11b2
Pranav Oak <pranavoak3101@gmail.com>
2026-06-23 12:51:38 +0000

    Jun 23, 2026, 6:21 PM

---
+++ Linear _DS/LinkedList/09DelNodeFromSpecificPos.c
@@ -0,0 +1,15 @@
+/*
+Assignment 9: The Node Deletion Challenge
+Incorporate a robust node deletion function into your current working Singly Linked List codebase.
+
+Requirements:
+Implement the Delete Function: Write bool deleteAtPosition(Node headRef, int position);. It should return 
+true on successful deletion and false if the position is out of bounds or the list is completely empty.
+
+Interactive Menu Expansion: Add a new option to your main() loop: 
+    4) Delete at Position (and shift your exit option to 5).
+
+Scan Validation: Prompt the user for the target index to delete and protect
+     the entry using your standard scanf verification logic.
+
+*/
