83ca539e28f129a5c90bd7393aac64e275152a7e
Pranav Oak <pranavoak3101@gmail.com>
2026-06-24 16:35:22 +0000

    Jun 24, 2026, 10:05 PM

--- Linear _DS/LinkedList/09DelNodeFromSpecificPos.c
+++ Linear _DS/LinkedList/09DelNodeFromSpecificPos.c
@@ -270,23 +270,25 @@ bool deleteAtPosition(node ** headRef,int position)
         temp = temp->next;
     }
 
-    //Check for Bound Contditions
-    if (temp == NULL) {
-    printf("Error: Position out of bounds.\n");
-    return false;
-}
+    // Check for Bound Contditions
+    if (temp == NULL)
+    {
+        printf("Error: Position out of bounds.\n");
+        return false;
+    }
 
-    //Scenario 4: removal of tail node
-    if(temp->next == NULL)
+    // Scenario 4: removal of tail node
+    if (temp->next == NULL)
     {
         current->next = NULL;
         free(temp);
         return true;
     }
-    else{
-    current ->next = temp->next;
-    free(temp);
-    return true;
+    else
+    {
+        current->next = temp->next;
+        free(temp);
+        return true;
     }
 }
 
