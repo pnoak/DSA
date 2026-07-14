4e9b859a87293d3b3f51bcd716dc50933ab1661e
Pranav Oak <pranavoak3101@gmail.com>
2026-07-14 19:12:21 +0530

    Implement circular linked list with memory allocation, traversal, and deallocation functions

--- Linear _DS/LinkedList/Circular_LinkedList/01circularLinkedListImplementation.c
+++ Linear _DS/LinkedList/Circular_LinkedList/01circularLinkedListImplementation.c
@@ -19,3 +19,108 @@
 
 */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct CirNode{
+    int data;
+    struct CirNode* next;
+}node;
+
+//Fucntion Declarations
+
+void displayList(node* head);
+void freeList(node* head);
+
+int main()
+{
+    // Creation of node heads 
+    node* node1= NULL;
+    node* node2 = NULL;
+    node* node3 = NULL;
+    node* head = NULL;
+
+    // Make a node 
+    node1 = (node*)malloc(sizeof(node));
+    node2 = (node*)malloc(sizeof(node));
+    node3 = (node*)malloc(sizeof(node));
+
+    // check the allocated memory for initialization status 
+    if(node1 == NULL || node2 == NULL || node3 == NULL)
+    {
+        fprintf(stderr,"Error: Memory Allocation Failed \n");
+        free(node1);
+        free(node2);
+        free(node3);
+        return EXIT_FAILURE;
+    }
+
+    head = node1;
+
+    // Initialize the node with data 
+    node1->data = 100;
+    node2->data = 200;
+    node3->data = 300;
+
+    // Wire the nodes to form a circular linked list
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node1;
+
+    //List Traversal and display of teh List data
+    displayList(head);
+
+    // Free-up the whole list and deallocate the heap memory
+    
+    freeList(head);
+    
+    node1= NULL;
+    node2= NULL;
+    node3= NULL;
+    head = NULL;
+
+    return EXIT_SUCCESS;
+}
+
+void displayList(node* head)
+{
+    if(head == NULL)
+    {
+        fprintf(stderr,"Error:List is Empty..\n");
+        return;
+    }
+
+    node* current = head;
+    int cnt=0;
+    do
+    {
+        printf("Node%d: %d \n",cnt,current->data);
+        cnt++;
+        current = current->next;
+    } while (current != head);
+    
+}
+
+void freeList(node* head)
+{
+    if(head == NULL)
+    {
+        printf("There is no element in the list\n");
+        return;
+    }
+
+    node* current = head;
+    node*temp = NULL;
+    int cnt=0;
+    do
+    {
+        temp = current->next;
+        free(current);
+        current = temp;
+        printf("Node %d frees..\n",cnt);
+        cnt++;
+    } while (current != head);
+
+    printf("All nodes freed..\n");
+}
